208e8628b7510440b7a60897a95c834509c136b4
angelo-carniel <angelofilho.e.t.d@gmail.com>
2021-12-17 23:32:25 -0300

    Site de E-commerce

---
+++ atividade02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            float dataNascimento, idade;
+            Console.WriteLine("Informe seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Informe sua data de nascimento:");
+            dataNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe sua idade:");
+            idade = int.Parse(Console.ReadLine());
+            if(idade>=18)
+            {
+                    Console.WriteLine("Sua senha é "+nome +dataNascimento);
+            }
+                        else
+            {            
+                    Console.WriteLine("Sua senha é "+dataNascimento +nome);
+            }
+        }
+    }
+}
+
